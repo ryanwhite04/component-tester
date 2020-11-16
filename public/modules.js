2558afe98e6aedecaa56fa96d8d80c35276f4eca
Ryan White <ryanwhite04@gmail.com>
2020-11-16 17:27:20 +0800

    Include wired-combo, wired-item, and wired-card

--- public/modules.js
+++ public/modules.js
@@ -3,4 +3,7 @@ import rough from 'roughjs/bundled/rough.esm.js';
 import { LitElement, html, css } from 'lit-element/lit-element.js';
 import 'wired-button';
 import 'wired-slider';
+import 'wired-combo';
+import 'wired-item';
+import 'wired-card';
 export { Port, rough, LitElement, html, css }
