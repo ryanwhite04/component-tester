09bf222cdef19fe5af81986521ef6aba510b0a7b
Ryan White <ryanwhite04@gmail.com>
2020-11-16 14:01:49 +0800

    Increase component drawing visibility

--- public/index.js
+++ public/index.js
@@ -5,12 +5,14 @@ class IntegratedCircuit extends LitElement {
       connected: { type: Boolean },
       width: { type: Number },
       height: { type: Number },
+      rasterized: { type: Boolean },
     }
   }
   constructor() {
     super();
     this.connected = false;
     this.width = this.height = 192;
+    this.rasterized = false;
   }
   async request() {
     return new Port(await navigator.usb.requestDevice({ filters: [
@@ -76,23 +78,35 @@ class IntegratedCircuit extends LitElement {
     }
   }
   firstUpdated() {
-    this.drawComponent(this.shadowRoot.getElementById('svg'), this.width, this.height);
+    this.drawComponent(this.shadowRoot.getElementById('component'), this.width, this.height);
   }
-  drawComponent(svg, w, h) {
-    [...svg.children].map(child => child.remove());
-    let rs = rough.svg(svg);
-    console.log(rs);
-    svg.appendChild(rs.rectangle(w/4, h/8, w/2, 6*h/8));
+  drawComponent(component, w, h) {
+    let rs = rough[this.rasterized ? 'canvas' : 'svg'](component, { options: {
+      strokeWidth: 3,
+      roughness: 1,
+    }});
+    let parts = [];
+    if (this.rasterized) {
+      component.getContext('2d').clearRect(0, 0, w, h);
+    } else {
+      [...component.children].map(child => child.remove());   
+    }
+    parts.push(rs.rectangle(w/4, h/8, w/2, 6*h/8, {
+      fill: 'black',
+      fillStyle: 'cross-hatch',
+      hachureGap: 8,
+      hachureAngle: 45,
+    }));
     for (let i = 0; i < 7; i++) {
       let y = h/8 + (i+1)*3*w/(4*8);
-      svg.appendChild(rs.line(w/8, y, w/4, y)); // x1, y1, x2, y2
-      svg.appendChild(rs.line(3*h/4, y, 7*h/8, y)); // x1, y1, x2, y2  
-
+      parts.push(rs.line(w/8, y, w/4, y, { stroke: '#777' })); // x1, y1, x2, y2
+      parts.push(rs.line(3*h/4, y, 7*h/8, y, { stroke: '#777' })); // x1, y1, x2, y2  
     }
+    this.rasterized || parts.map(part => component.appendChild(part))  
   }
   static get styles() {
     return css`
-      svg {
+      #component {
         display: block;
         margin: auto;
       }
@@ -103,7 +117,11 @@ class IntegratedCircuit extends LitElement {
       <wired-button @click=${this.test}>Test</wired-button>
       <wired-button @click=${this.toggle}>${this.connected ? "Disconnect" : "Connect"}</wired-button>
       <p>Component</p>
-      <svg width=${this.width} height=${this.height} id="svg"></svg>
+      ${this.rasterized ? 
+        html`<canvas width=${this.width} height=${this.height} id="component"></canvas>` :
+        html`<svg width=${this.width} height=${this.height} id="component"></svg>`
+      }
+      
     `;
   }
 }
